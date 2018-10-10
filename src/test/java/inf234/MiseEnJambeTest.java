b5454a1968338ac2fc6d0e957554ddabf492bb9c
Theo <theo.rey.viviant@gmail.com>
2018-10-10 12:55:34 +0200

    Update MiseEnJambeTest.java

--- src/test/java/inf234/MiseEnJambeTest.java
+++ src/test/java/inf234/MiseEnJambeTest.java
@@ -12,43 +12,4 @@ public class MiseEnJambeTest {
         assertTrue(m1.contient("uvw</"));
     }
 
-/* -------------------------------------------------------------------------------------- */
-
-    public void testContientBalise1() throws Exception { //Test standard
-        MiseEnJambe m1 = new MiseEnJambe("<a>xyz<bcd>uvw</bcd>toto</a>"); 
-        assertTrue(m1.contientBalise("bcd"));
-        assertFalse(m1.contientBalise("xyz"));
-    }
-
-    public void testContientBalise1() throws Exception { //Test de non appartenance
-        MiseEnJambe m1 = new MiseEnJambe("<a>xyzuvwtoto</a>");
-        assertFalse(m1.contientBalise("bcd"));.
-        assertTrue(m1.contientBalise("zu"));  
-    }
-
-    public void testContientBalise2() throws Exception { //Test des positions extremes des balise
-        MiseEnJambe m1 = new MiseEnJambe("<bcd>xyz<a>uvw</a>toto</bcd>");
-        assertTrue(m1.contientBalise("bcd"));  
-    }
-
-    public void testContientBalise3() throws Exception { //Test des imbrications des balises
-        MiseEnJambe m1 = new MiseEnJambe("<bcd><bcd><bcd>fkjzefzhj</bcd></bcd></bcd>");
-        assertTrue(m1.contientBalise("bcd"));  
-    }
-
-    public void testContientBalise4() throws Exception { //Test de balises enchainne
-        MiseEnJambe m1 = new MiseEnJambe("<bcd></bcd><bcd>fkjzefzhj</bcd><bcd></bcd>");
-        assertFalse(m1.contientBalise("bcd"));  
-    }
-
-    public void testContientBalise5() throws Exception {
-        MiseEnJambe m1 = new MiseEnJambe("<a>xyz<bcd>uvw<b>toto</a");
-        assertFalse(m1.contientBalise("bcd"));  
-    }
-
-    public void testContientBalise5() throws Exception {
-        MiseEnJambe m1 = new MiseEnJambe("<bcd>xyz<a>uvw<a>toto</a");
-        assertTrue(m1.contientBalise("bcd"));  
-    }
-
 }
